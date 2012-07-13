4432f243bf7143135ad47d99cf1f355c1d51db84
pcherenkov <pcherenkov@gmail.com>
2012-07-13 16:27:54 +0400

    added debug output to fiber_cond implementation

--- src/fiber_cond.m
+++ src/fiber_cond.m
@@ -27,6 +27,7 @@
 
 #include <unistd.h>
 #include <sys/types.h>
+#include <errno.h>
 
 #include <tarantool.h>
 #include TARANTOOL_CONFIG
@@ -86,7 +87,7 @@ valid_dgram(struct cond_dgram *dgram)
 		return true;
 	} while(0);
 
-	say_error("datagram verification failed: D=[op=%c, index=%d], cond.index=%d\n",
+	say_error("Datagram verification failed: D=[op=%c, index=%d], cond.index=%d\n",
 		dgram->op, dgram->index, fcond_registry.cond[dgram->index - 1].index);
 	return false;
 }
@@ -98,19 +99,31 @@ fiber_cond_cb(ev_watcher *watcher __attribute__((unused)), int event __attribute
 	struct cond_dgram dgram = {'\0', 0};
 	ssize_t nrd = -1;
 
+	say_debug("Fiber condition event: watcher=%p, event=%d", (void*)watcher, event);
 	assert(fiber == &sched);
 
-	while ((nrd = read(fcond_registry.pipefd[0], &dgram, sizeof(dgram))) > 0) {
+	while (1) {
+		nrd = read(fcond_registry.pipefd[0], &dgram, sizeof(dgram));
+		if (nrd == -1 && (errno == EAGAIN || errno == EWOULDBLOCK))
+			break;
 		if (nrd != (ssize_t)sizeof(dgram)) {
 			say_error("Read %ld bytes, expected to get %ld\n", (long)nrd,
 				(long)sizeof(dgram));
 			break;
 		}
+		say_debug("datagram read: op=%c, index=%d", dgram.op, dgram.index);
 
-		if (!valid_dgram(&dgram))
+		if (!valid_dgram(&dgram)) /* TODO: corrupt structure is a fatal error. */
 			break;
 		
 		struct fiber_cond *fcond = &fcond_registry.cond[dgram.index - 1];
+		say_debug("%s: [%c] condition=%p", __func__, dgram.op, (void*)fcond);
+
+		if (!fcond->blocked) {
+			say_debug("%s: no fibers are blocked on condition %p, skipping.",
+				__func__, (void*)fcond);
+			continue;
+		}
 
 		if (dgram.op == 'B') {
 			/* Call all blocked fibers, mark the refs processed. */
@@ -118,8 +131,12 @@ fiber_cond_cb(ev_watcher *watcher __attribute__((unused)), int event __attribute
 					*ref = NULL;
 
 			for(ref = head; ref != NULL; ref = ref->next) {
+				say_debug("%s: waking up fiber %p (broadcast)",
+					__func__, (void*)ref->target);
 				fiber_call(ref->target);
+
 				ref->target = NULL; /* Mark it. */
+				say_debug("%s: marking up fiber %p", __func__, (void*)ref->target);
 			}
 
 			/* Find the first ref with ref->target == NULL, save the preceding ref. */
@@ -129,6 +146,8 @@ fiber_cond_cb(ev_watcher *watcher __attribute__((unused)), int event __attribute
 			/* Remove all after the first marked ref. */
 			while(ref) {
 				assert(ref->target);
+				say_debug("%s: removing fiber %p from condition %p",
+					__func__, (void*)ref->target, (void*)fcond);
 				struct fiber_ref *tmp = ref->next;
 				free(ref); /* TODO */
 				ref = tmp;
@@ -137,18 +156,15 @@ fiber_cond_cb(ev_watcher *watcher __attribute__((unused)), int event __attribute
 			/* Trim the list. */
 			if (prev)
 				prev->next = NULL;
-
-			/* Adjust the head. */
-			if (prev == fcond->blocked)
+			else
 				fcond->blocked = NULL;
-
 		}
 		else if (dgram.op == 'S') {
-			if (!fcond->blocked)
-				break;
-
 			/* Grab the current head and call the referenced fiber. */
 			struct fiber_ref *head = fcond->blocked;
+
+			say_debug("%s: waking up fiber %p (signal)",
+					__func__, (void*)head->target);
 			fiber_call(head->target);
 
 			/* Find the element preceding the head
@@ -157,16 +173,20 @@ fiber_cond_cb(ev_watcher *watcher __attribute__((unused)), int event __attribute
 			for(struct fiber_ref *ref = fcond->blocked;
 				ref && ref != head; prev = ref, ref = ref->next);
 
-			/* Remove the (former) head. */
+			/* Trim the list. */
 			if (prev)
 				prev->next = head->next;
-
-			free(head);
-
-			/* Adjust the head. */
-			if (head == fcond->blocked)
+			else
 				fcond->blocked = NULL;
+
+			/* Remove the (former) head. */
+			free(head);
 		}
+
+		say_debug("%s: %s more fibers blocked on cond %p",
+			__func__, fcond->blocked ? "still" : "no",
+			(void*)fcond);
+
 	} /* while */
 
 	return;
@@ -176,21 +196,21 @@ fiber_cond_cb(ev_watcher *watcher __attribute__((unused)), int event __attribute
 void
 fiber_cond_global_init()
 {
-	int rc = pipe(fcond_registry.pipefd);
-	assert (rc == 0); /* TODO: add proper handling */
-	(void) rc;
-
-	if (	set_nonblock(fcond_registry.pipefd[0]) != 0 ||
-		set_nonblock(fcond_registry.pipefd[0]) != 0)
-			assert(false); /* TODO: add proper handling */
+	if (pipe(fcond_registry.pipefd) != 0 ||
+	     set_nonblock(fcond_registry.pipefd[0]) != 0 ||
+	     set_nonblock(fcond_registry.pipefd[0]) != 0)
+			panic("Error setting up fiber-condition "
+				"event pipe");
 
 	ev_io_init(&fcond_registry.io, (void*)&fiber_cond_cb,
 		fcond_registry.pipefd[0], EV_READ);
 
-	/* TODO: we could maintain a counter and enable the relevant fd
-	 * only when needed, but to simplify things *right now* we won't.
+	/* We could maintain a usage counter and enable the read-pipe fd
+	 * only as needed, but to simplify things *right now* we won't.
 	 */
 	 ev_io_start(&fcond_registry.io);
+
+	 say_debug("%s: done", __func__);
 }
 
 
@@ -199,10 +219,12 @@ fiber_cond_global_destroy()
 {
 	ev_io_stop(&fcond_registry.io);
 
-	fiber_cond_destroy_all(); /* TODO: implement */
+	fiber_cond_destroy_all();
 
 	(void) close(fcond_registry.pipefd[0]);
 	(void) close(fcond_registry.pipefd[1]);
+
+	say_debug("%s: done", __func__);
 }
 
 
@@ -223,6 +245,9 @@ fiber_cond_init(struct fiber_cond **fcond)
 	fcond_registry.cond[i].blocked = NULL;
 
 	*fcond = &fcond_registry.cond[i];
+
+	say_debug("%s: condition %p [index=%d] created", __func__, (void*)*fcond,
+		fcond_registry.cond[i].index);
 	return 0;
 }
 
@@ -233,14 +258,23 @@ fiber_cond_destroy(struct fiber_cond *fcond)
 	assert(fcond->index > 0);
 
 	if (fcond->blocked != NULL) {
-		say_warn("Destroying fiber condition with waiters.");
+		say_warn("Destroying fiber condition %p with waiting fibers.",
+			(void*)fcond);
+		/* TODO: what do we do in such a case? wake fibers up or not? */
 
 		for (struct fiber_ref *ref = fcond->blocked; ref != NULL; ) {
 			struct fiber_ref *tmp = ref->next;
+
+			say_debug("%s: fiber %p will be detached from condition %p",
+				__func__, (void*)ref->target, (void*)fcond);
+
 			free(ref); /* TODO: malloc/free */
 			ref = tmp;
 		}
 	}
+
+	say_debug("%s: destroyed condition %p [index=%d]", __func__,
+		(void*)fcond, fcond->index);
 
 	fcond->index  = 0; /* Unused now. */
 	return 0;
@@ -262,7 +296,8 @@ fiber_cond_wait(struct fiber_cond *fcond)
 	assert(fcond->index > 0);
 
 	struct fiber_ref *ref = calloc(1, sizeof(ref)); /* TODO: malloc */
-	assert(ref); /* TODO: handle properly */
+	if (ref == NULL)
+		panic("%s: failed to create fiber condition reference", __func__);
 
 	ref->target = fiber; /* Current fiber. */
 
@@ -270,9 +305,14 @@ fiber_cond_wait(struct fiber_cond *fcond)
 	ref->next = fcond->blocked;
 	fcond->blocked = ref;
 
+	say_debug("%s: fiber %p will now block on condition %p",
+		__func__, (void*)fiber, (void*)fcond);
+
 	fiber_yield();
 	fiber_testcancel();
 
+	say_debug("%s: fiber %p woke up on condition %p",
+		__func__, (void*)fiber, (void*)fcond);
 	return 0;
 }
 
@@ -288,11 +328,14 @@ fiber_cond_raise(char op, struct fiber_cond *fcond)
 	assert(fcond->index > 0);
 	dgram.index = fcond->index;
 
-	/* TODO: make sure the pipe is non-blocking */
-	ssize_t nwr = write(fcond_registry.pipefd[1], &dgram, sizeof(dgram));
-	assert(nwr == sizeof(dgram)); /* TODO: proper handling */
+	say_debug("%s: raising [%c] condition %p", __func__, op, (void*)fcond);
 
-	(void) nwr;
+	ssize_t nwr = write(fcond_registry.pipefd[1], &dgram, sizeof(dgram));
+	if (nwr != sizeof(dgram)) {
+		say_error("Error writing to fiber-condition pipe, nwr=%ld",
+			(long)nwr);
+		return -1;
+	}
 	return 0;
 }
 
